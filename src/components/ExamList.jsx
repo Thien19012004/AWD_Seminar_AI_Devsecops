f23fd33a229aebfcf15e75f6a9c2d9af35504831
22120343 <ducthien19012004@gmail.com>
2025-12-03 19:14:46 +0700

    add dast stage

--- src/components/ExamList.jsx
+++ src/components/ExamList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";ddddd
+import React, { useState, useEffect } from "react";
 import authService from "../services/authService";
 import { useNavigate } from "react-router-dom";
 import "../styles/global.css";
@@ -25,7 +25,7 @@ function ExamList() {
     handleExamList();
   }, []);
 
-  // fetch exam list every 2 minutes----------------------------
+   fetch exam list every 2 minutes----------------------------
   // useEffect(() => {
   //   const interval = setInterval(() => {
   //     handleExamList();
